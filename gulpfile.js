a7aad67846ede6d23d443b4af8107fa5a4e58b6d
Roméo Jr. Labonté <romeo@r42.ca>
2016-03-29 16:42:34 -0400

    fixed compilation & typo

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const del = require('del');
 const gulp = require('gulp');
 const jade = require('gulp-jade');
 const less = require('gulp-less');
@@ -10,15 +11,19 @@ const jadeOutputPath = path.resolve('./views/source');
 const lessOutputPath = path.resolve('./css/source');
 
 gulp.task('jade', () => {
-	return gulp.src(path.resolve(jadeOutputPath, '..', 'index.jade'))
+	return gulp.src(path.resolve(jadeOutputPath, 'index.jade'))
 			.pipe(jade())
-			.pipe(gulp.dest(jadeOutputPath));
+			.pipe(gulp.dest(path.resolve(jadeOutputPath, '..')));
 });
 
 gulp.task('less', () => {
 	return gulp.src(path.resolve(lessOutputPath, 'master.less'))
 			.pipe(less())
 			.pipe(gulp.dest(path.resolve(lessOutputPath, '..')));
+});
+
+gulp.task('clean:html', () => {
+	return del([path.resolve(jadeOutputPath, '..', 'index.html')]);
 });
 
 gulp.task('watch', () => {
